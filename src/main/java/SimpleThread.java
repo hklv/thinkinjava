cb24679832bff1987d472e1ca033bffa962bba12
hklv <lvhuikang@gmail.com>
2016-07-15 07:55:07 +0800

    simpleThread

---
+++ src/main/java/SimpleThread.java
@@ -0,0 +1,30 @@
+/**
+ * Created by hklv on 2016/7/15.
+ */
+public class SimpleThread extends Thread {
+    private int countDown = 5;
+    private static int threadCount = 0;
+
+    public SimpleThread() {
+        super(Integer.toString(++threadCount));
+        start();
+    }
+
+    public String toString() {
+        return "#" + getName() + "(" + countDown + ").";
+    }
+
+    public void run() {
+        while (true) {
+            System.out.println(this);
+            if (--countDown > 0)
+                return;
+        }
+    }
+
+    public static void main(String[] arsg) {
+        for (int i = 0; i < 5; i++) {
+            new SimpleThread();
+        }
+    }
+}
